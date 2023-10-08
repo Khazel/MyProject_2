442259f62a6717e783439c121e8a9b40fd8b2be4
Deathclaw <pthrfkj66@gmail.com>
2023-10-08 14:13:22 +0300

    Updated experienced lector minimum age to be a constant.

--- src/Lecture_8/Homework_8/Lector.java
+++ src/Lecture_8/Homework_8/Lector.java
@@ -2,6 +2,8 @@ package Lecture_8.Homework_8;
 
 public class Lector extends Person {
     private double dailySalary;
+    final int experiencedLectorMinAge = 30;
+
     public Lector(String firstName, String lastName, int age, String gender, double dailySalary){
         super(firstName, lastName, age, gender);
         this.dailySalary = dailySalary;
@@ -12,7 +14,7 @@ public class Lector extends Person {
     }
     public double calculateOvertime(double hours) {
         double overtimeAmout = 0;
-        if (getAge() >= 30) {
+        if (getAge() >= experiencedLectorMinAge) {
             overtimeAmout += (hours * (dailySalary * 2));
         }
         return overtimeAmout;
